80ccae5d229f8d983fd0264f9f6849a5a29e7003
Luke McMeans <elukemcmeans@gmail.com>
2024-04-09 17:27:37 -0400

    adding all 8 players

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -21,5 +21,5 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.homepage_view, name="homepage"),
-    path('', views.submit, name="submit"),
+    path('submit/', views.submit, name="submit"),
 ]
