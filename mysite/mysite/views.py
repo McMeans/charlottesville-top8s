75e667de258531a7bd9ae6d223d05f69e638e4df
Luke McMeans <elukemcmeans@gmail.com>
2024-04-13 20:15:45 -0400

    Stopping point 4/13

--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.template.loader import get_template
 from .models import Player, Event
 from datetime import datetime
+from PIL import Image, ImageDraw, ImageFont
+import os
 
 def homepage_view(request):
     return render(request, 'mysite/homepage.html')
@@ -62,8 +64,44 @@ def submit(request):
         sideTitle = request.POST.get('side_event').strip()
         sideWinner = request.POST.get('side_name').strip()
     event = Event.objects.create(event_title=title, event_participants=participants, event_date=date, side_title=sideTitle, side_winner=sideWinner, redemption_winner=redempWinner, redemption_redner=redempRender)
-    return constructGraphic(top_players, event)
+    if title.startswith("Smash"):
+        return constructSmashAtUVA(top_players, event)
+    else:
+        return constructCUT(top_players, event)
 
-def constructGraphic(top_players, event):
+def constructSmashAtUVA(top_players, event):
+    graphic = Image.new("RGB", (1920,1080))
+    start_color = (35, 45, 75)
+    end_color = (18, 24, 40) 
+    gradient = [(int(start_color[0] + (end_color[0] - start_color[0]) * i / 1080),
+                int(start_color[1] + (end_color[1] - start_color[1]) * i / 1080),
+                int(start_color[2] + (end_color[2] - start_color[2]) * i / 1080)) for i in range(1080)]
+    graphic.put(gradient * 1920)
+    font = None #TODO: ADD THIS
+    
+    #TODO: ADD BACKGROUND IMAGE
+
+    #TODO: ADD TITLE
+
+    graphic = addPlayers(top_players, event, graphic, font)
+
+    #TODO: Implement the rest
+    return None
+
+def constructCUT(top_players, event):
+    graphic = Image.new("RGB", (1920,1080))
+    font = None #TODO: ADD THIS
+
+    #TODO: ADD BACKGROUND IMAGE
+
+    #TODO: ADD TITLE
+    
+    graphic = addPlayers(top_players, event, graphic, font)
+
+    #TODO: Implement the rest
+    return None
+
+
+def addPlayers(top_players, event, graphic, font):
     #TODO: Actually implement this
     return None
