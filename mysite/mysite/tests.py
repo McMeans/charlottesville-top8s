0ac1393736fa6b8b32e4ac19582a5bcb62a84190
Luke McMeans <elukemcmeans@gmail.com>
2024-04-14 22:24:31 -0400

    More progress on the actual photo graphic

--- mysite/mysite/tests.py
+++ mysite/mysite/tests.py
@@ -1,25 +1,12 @@
 from PIL import Image, ImageDraw, ImageFont
 
-graphic = Image.new("RGB", (1920,1080))
-start_color = (35, 45, 75)
-end_color = (18, 24, 40) 
-for y in range(1080):
-    color = (
-        int(start_color[0] + (end_color[0] - start_color[0]) * y / 1080),
-        int(start_color[1] + (end_color[1] - start_color[1]) * y / 1080),
-        int(start_color[2] + (end_color[2] - start_color[2]) * y / 1080)
-    )
-    for x in range(1920):
-        graphic.putpixel((x, y), color)
+graphic = Image.new("RGBA", (1920,1080))
 
-background_image = Image.open('mysite/mysite/uva_sunrise_aerial.jpg')
-background_opacity = background_image.convert("RGBA")
-opacity_layer = Image.new("RGBA", background_opacity.size, (255, 255, 255, int(255 * 0.9)))
-background_opacity = Image.alpha_composite(background_opacity, opacity_layer)
-graphic.paste(background_opacity, (0,0), background_opacity)
+background_image = Image.open('mysite/static/images/backgrounds/uva_fall_background.png')
+graphic.paste(background_image, (0,0))
 
 draw = ImageDraw.Draw(graphic)
-border_color = (256, 256, 256)
+border_color = (255, 255, 255)
 
 coords = [[35, 770, 632, 937],
           [683, 564, 1050, 666],
@@ -48,12 +35,17 @@ for index, coord in enumerate(coords):
     draw.rectangle((x1, y1, x2, y2), outline=border_color, width=4)
 
 text = "Smash @ UVA S24 #12"
-font_name = "Arial Bold Italic"
+font_path = 'mysite/static/fonts/Roboto-BoldItalic.ttf'
 font_size = 96
 text_color = (255, 255, 255)
-shadow_color = (0, 0, 0, 20)
-font = ImageFont.truetype(font_name + ".ttf", font_size)
-draw.text((40,25), text, font=font, fill=shadow_color)
-draw.text((35,20), text, font=font, fill=text_color)
+shadow_color = (0, 0, 0, 255)
+font = ImageFont.truetype(font_path, font_size)
+draw.text((48,23), text, font=font, fill=shadow_color)
+draw.text((45,20), text, font=font, fill=text_color)
+
+font = ImageFont.truetype(font_path, 48)
+text = 'Top 8 - 04/12/24'
+draw.text((48,138), text, font=font, fill=shadow_color)
+draw.text((45,135), text, font=font, fill=text_color)
 # Display the image
 graphic.show()
