a9e884fc6ae6b76d7aa0342574f235d592a071c6
Luke McMeans <elukemcmeans@gmail.com>
2024-04-15 21:43:52 -0400

    Adding images

--- mysite/mysite/tests.py
+++ mysite/mysite/tests.py
@@ -8,6 +8,47 @@ graphic.paste(background_image, (0,0))
 draw = ImageDraw.Draw(graphic)
 border_color = (255, 255, 255)
 
+char = 'Cloud'
+mario0 = Image.open(f'mysite/static/images/renders/{char}/{char}_0.png')
+size = ((599),(599))
+mario0 = mario0.resize(size)
+graphic.alpha_composite(mario0, (34,271))
+
+mario1 = Image.open(f'mysite/static/images/renders/{char}/{char}_1.png')
+size = ((369),(369))
+mario1 = mario1.resize(size)
+graphic.alpha_composite(mario1, (682,246))
+
+mario2 = Image.open(f'mysite/static/images/renders/{char}/{char}_2.png')
+size = ((369),(369))
+mario2 = mario2.resize(size)
+graphic.alpha_composite(mario2, (1099,246))
+
+mario3 = Image.open(f'mysite/static/images/renders/{char}/{char}_3.png')
+size = ((369),(369))
+mario3 = mario3.resize(size)
+graphic.alpha_composite(mario3, (1516,246))
+
+mario4 = Image.open(f'mysite/static/images/renders/{char}/{char}_4.png')
+size = ((276),(276))
+mario4 = mario4.resize(size)
+graphic.alpha_composite(mario4, (684,667))
+
+mario5 = Image.open(f'mysite/static/images/renders/{char}/{char}_5.png')
+size = ((276),(276))
+mario5 = mario5.resize(size)
+graphic.alpha_composite(mario5, (996,667))
+
+mario6 = Image.open(f'mysite/static/images/renders/{char}/{char}_6.png')
+size = ((276),(276))
+mario6 = mario6.resize(size)
+graphic.alpha_composite(mario6, (1301,667))
+
+mario7 = Image.open(f'mysite/static/images/renders/{char}/{char}_7.png')
+size = ((276),(276))
+mario7 = mario7.resize(size)
+graphic.alpha_composite(mario7, (1608,667))
+
 coords = [[34, 769, 633, 938],
           [682, 563, 1051, 667],
           [1099, 563, 1468, 667],
