5f87a1bfe7e00bd227d4036174b6ce543201bb0d
Luke McMeans <elukemcmeans@gmail.com>
2024-05-18 16:18:12 -0700

    adjusting models

--- mysite/mysite/models.py
+++ mysite/mysite/models.py
@@ -3,11 +3,11 @@ from django.db import models
 
 class Player(models.Model):
     player_name = models.CharField(max_length=40)
-    player_handle = models.CharField(max_length=16)
+    player_handle = models.CharField(max_length=16, null=True)
     player_placement = models.IntegerField()
     primary_character = models.ImageField()
-    secondary_character = models.ImageField()
-    tertiary_character = models.ImageField()
+    secondary_character = models.ImageField(null=True)
+    tertiary_character = models.ImageField(null=True)
     
     class Meta:
         app_label = 'mysite'
@@ -20,10 +20,10 @@ class Event(models.Model):
     event_title = models.CharField(max_length=20)
     event_participants = models.IntegerField()
     event_date = models.CharField(max_length=10)
-    side_title = models.CharField(max_length=30)
-    side_winner = models.CharField(max_length=80)
-    redemption_winner = models.CharField(max_length=40)
-    redemption_render = models.ImageField()
+    side_title = models.CharField(max_length=30, null=True)
+    side_winner = models.CharField(max_length=80, null=True)
+    redemption_winner = models.CharField(max_length=4, null=True)
+    redemption_render = models.ImageField(null=True)
 
     class Meta:
         app_label = 'mysite'
