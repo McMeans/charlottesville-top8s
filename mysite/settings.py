6567d18855196876038116678e165975a61cf1b6
Luke McMeans <elukemcmeans@gmail.com>
2025-03-29 10:28:58 -0400

    debug off

--- mysite/settings.py
+++ mysite/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@4lu^6sv6kn=%f#(*cplyy!33*@q=p366m%s$i)kl0mnv1m-_('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
